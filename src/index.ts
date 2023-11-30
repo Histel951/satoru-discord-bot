3f6745b1e4264816dd41d0068d19c2666f8911f0
Histel951 <histel951@gmail.com>
2023-11-30 17:53:34 +0300

    init

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
-import { Client, GatewayIntentBits } from "discord.js";
+import { Client, GatewayIntentBits, REST, Routes } from "discord.js";
+import 'dotenv/config';
+
 
 const client = new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]
 });
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    if (interaction.commandName === 'ping') {
+        await interaction.reply('Pong!');
+    }
+});
+
+client.login(process.env.DISCORD_TOKEN);
