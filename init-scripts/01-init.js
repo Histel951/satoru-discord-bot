357f2064304050b138690e22876d8d931232b5f0
Histel951 <histel951@gmail.com>
2024-04-09 12:39:40 +0300

    [docker] сделал развёртывание api контейнера

--- init-scripts/01-init.js
+++ init-scripts/01-init.js
@@ -1,7 +1,1 @@
 db.auth(process.env.MONGO_INITDB_ROOT_USERNAME, process.env.MONGO_INITDB_ROOT_PASSWORD)
-
-const satoroDb = db.getSiblingDB(process.env.MONGO_INITDB_DATABASE);
-
-// базовые коллекции для этой бд
-satoroDb.createCollection('teams')
-satoroDb.createCollection('players')
