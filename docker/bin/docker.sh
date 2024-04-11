5615948261698393c3c23e24fec859265c4b33d2
Histel951 <histel951@gmail.com>
2024-04-11 11:07:08 +0300

    [docker] поправил docker

--- docker/bin/docker.sh
+++ docker/bin/docker.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
-if [ ! -d "$DOCKER_PROJECT_DIR/node_modules" ]; then
-    npm install --prefix "$DOCKER_PROJECT_DIR"
-fi
-
 npm run dev --prefix "$DOCKER_PROJECT_DIR"
 sh "/usr/local/bin/wait-for.sh" mongodb:"$MONGO_PORT"
